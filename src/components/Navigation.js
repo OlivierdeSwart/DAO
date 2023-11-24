c913cf9767b8d12520a17a579873192c3811189c
Olivier de Swart <olivierdeswart@gmail.com>
2023-11-24 11:54:23 +0100

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,28 @@
+import Navbar from 'react-bootstrap/Navbar';
+
+import logo from '../logo.png';
+
+const Navigation = ({ account }) => {
+
+  const accountDisplay = account ? `${account.slice(0, 5)}...${account.slice(38, 42)}` : '';
+
+  return (
+    <Navbar className='my-3'>
+      <img
+        alt="logo"
+        src={logo}
+        width="40"
+        height="40"
+        className="d-inline-block align-top mx-3"
+      />
+      <Navbar.Brand href="#">Dapp University DAO</Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          { accountDisplay }
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Navigation;
